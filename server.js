614afdfc9d66202f469bb2a6d839e50dee12bf94
ukrwebprom <ukrwebprom@gmail.com>
2023-05-14 15:22:56 +0300

    db connect & config.js

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const app = require('./app')
+const mongoose = require('mongoose');
+const {DB_PATH} = require('./config');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+
+mongoose.connect(DB_PATH)
+.then(() => {
+  console.log("Database connection successful");
+  app.listen(3000, () => {
+    console.log("Server running. Use our API on port: 3000")
+  })
 })
+.catch ((err) => {
+  console.log(err.message);
+  process.exit(1);
+  }
+)
